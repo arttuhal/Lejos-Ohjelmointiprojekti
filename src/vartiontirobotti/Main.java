f01a0a437d4b16f724a548ccc26bbcba8e16a0c6
Miikka Oksanen <miikka.oksanen@pp1.inet.fi>
2018-08-22 16:34:08 +0300

    Created methods for controlling the robot

--- src/vartiontirobotti/Main.java
+++ src/vartiontirobotti/Main.java
@@ -2,25 +2,29 @@ package vartiontirobotti;
 
 import lejos.hardware.motor.Motor;
 import lejos.robotics.RegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.utility.Delay;
 
 public class Main {
 
 	public static void main(String[] args) {
-		final RegulatedMotor mC = Motor.C;
-		final RegulatedMotor mB = Motor.B;
+		RegulatedMotor mL = new EV3LargeRegulatedMotor(MotorPort.C);
+		RegulatedMotor mR = new EV3LargeRegulatedMotor(MotorPort.B);
 		
-		Motors motors = new Motors();
-		motors.Drive(mB, mC);
-		Motor.B.forward();
-		Motor.C.backward();
-		Delay.msDelay(1000);
-		Motor.C.forward();
-		Motor.B.backward();
-		Delay.msDelay(1000);
-		Motor.B.stop();
-		Motor.C.stop();
+		Motors motors = new Motors(mL, mR);
 		
+		motors.DriveStraigth(1450, true);
+		motors.StopAndRotate(false, 100);
+		//motors.DriveStraigth(700, true);
+		//motors.StopAndRotate(true, 200);
+		//motors.DriveStraigth(1500, true);
+		//motors.TurnWhileMoving(left, howMuch, howLong);
+		mL.startSynchronization();
+		mR.stop(true);
+		mL.stop(true);
+		mL.endSynchronization();
 	}
 
 }
--- src/vartiontirobotti/Motors.java
+++ src/vartiontirobotti/Motors.java
@@ -1,19 +1,80 @@
 package vartiontirobotti;
 
 import lejos.hardware.motor.*;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
 
 public class Motors {
-	private RegulatedMotor mC = Motor.C;
-	private RegulatedMotor mB = Motor.B;
-	private int speed = 1000;
+	private RegulatedMotor mL; //= new EV3LargeRegulatedMotor(MotorPort.C);
+	private RegulatedMotor mR; //= new EV3LargeRegulatedMotor(MotorPort.B);
+	//private int speed = 1000;
+	final int DEFAULTSPEED = 1000;
 	
-	public void Drive(RegulatedMotor mC, RegulatedMotor mB) {
-		this.mC = mC;
-		this.mB = mB;
-		this.mC.setSpeed(speed);
-		this.mB.setSpeed(speed);
-		mC.synchronizeWith(new RegulatedMotor[] { mB });
+	public Motors(RegulatedMotor mL, RegulatedMotor mR) {
+		this.mL = mL;
+		this.mR = mR;
+		mL.setSpeed(DEFAULTSPEED);
+		mR.setSpeed(DEFAULTSPEED);
+		mL.synchronizeWith(new RegulatedMotor[] { mR });
+	}
+	/*
+	public void Drive() {
+		mL.setSpeed(speed);
+		mR.setSpeed(speed);
 		
+		mL.startSynchronization();
+		mL.forward();
+		mR.backward();
+		mL.endSynchronization();
+		Delay.msDelay(1000);
+		mL.startSynchronization();
+		mL.backward();
+		mR.forward();
+		mL.endSynchronization();
+		Delay.msDelay(1000);
+		
+		
+		mR.stop(true);
+		mL.stop(true);
+	}*/
+	
+	public void DriveStraigth(int howLong, boolean forward) {
+		mL.startSynchronization();
+		if (forward) {
+			mL.forward();
+			mR.forward();
+		} else {
+			mL.backward();
+			mR.backward();
+		}
+		mL.endSynchronization();
+		Delay.msDelay(howLong);
+	}
+	
+	public void StopAndRotate(boolean left, int degrees) {
+		mL.startSynchronization();
+		if (left) {
+			mR.rotate(degrees);
+			mL.rotate(-degrees);
+		} else {
+			mL.rotate(degrees);
+			mR.rotate(-degrees);
+		}
+		mL.endSynchronization();
+	}
+	
+	public void TurnWhileMoving(boolean left, int howMuch, int howLong) {
+		mL.startSynchronization();
+		if (left) {
+			mR.setSpeed(DEFAULTSPEED + howMuch);
+		} else {
+			mL.setSpeed(DEFAULTSPEED + howMuch);
+		}
+		mL.forward();
+		mR.forward();
+		mL.endSynchronization();
+		Delay.msDelay(howLong);
 	}
 }
