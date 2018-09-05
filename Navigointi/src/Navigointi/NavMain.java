6d485dbe860bdaac6c874e0d33604648ef23b3f5
Arttu Halonen <arttuhal@metropolia.fi>
2018-09-05 16:11:20 +0300

    Testing the MovePilot method

--- Navigointi/src/Navigointi/NavMain.java
+++ Navigointi/src/Navigointi/NavMain.java
@@ -1,10 +1,31 @@
 package Navigointi;
 
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.navigation.*;
+
 public class NavMain {
-
+	
+	// TODO: Vaihda ketjut renkaisiin. Liikkuu tasaisemmin ja helpompi mitata.
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RegulatedMotor right = new EV3LargeRegulatedMotor(MotorPort.B);
+		RegulatedMotor left = new EV3LargeRegulatedMotor(MotorPort.C);
+		Wheel wheel1 = WheeledChassis.modelWheel(right, 3.12).offset(-8.9);
+		// offset on renkaan keskipisteen etäisyys robotin keskipisteestä,
+		// vasen miinusmerkkisenä
+		Wheel wheel2 = WheeledChassis.modelWheel(left, 3.12).offset(8.9);
+		Chassis chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
+		MovePilot pilotti = new MovePilot(chassis);
+		// metri eteenpäin
+		//pilotti.travel(100);
+		// käänny kolme kierrosta oikealle
+		pilotti.rotate(-1080);
+		// kaarta pitkin: 50 cm säde, 90 asteen keskuskulma
+		//pilotti.arc(50, 90);
 	}
-
 }
